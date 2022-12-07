bc3a906f2fe8aeeddd5751386337ce323923fac5
Santiago TRUJILLO <tsantiag@e2r11p1.clusters.42paris.fr>
2022-12-07 16:02:25 +0100

    my start of So_long

---
+++ So_Long/headers/solong.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solong.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsantiag <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 13:51:27 by tsantiag          #+#    #+#             */
+/*   Updated: 2022/12/07 14:37:11 by tsantiag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLONG_H
+# define SOLONG_H
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../headers/get_next_line.h"
+#include "../headers/mlx.h"
+
+typedef struct game{
+
+
+
+
+
+} t_complete
+
+
+#endif
